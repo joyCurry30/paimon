3bffa1d5ed843dbbcc2a172f149be98970be5986
tianzhu.wen <curry30play@gmail.com>
2024-05-17 11:30:00 +0800

    [core] AddColumn move support before and last.

--- paimon-core/src/main/java/org/apache/paimon/schema/SchemaManager.java
+++ paimon-core/src/main/java/org/apache/paimon/schema/SchemaManager.java
@@ -339,28 +339,7 @@ public class SchemaManager implements Serializable {
                 } else if (change instanceof UpdateColumnPosition) {
                     UpdateColumnPosition update = (UpdateColumnPosition) change;
                     SchemaChange.Move move = update.move();
-
-                    // key: name ; value : index
-                    Map<String, Integer> map = new HashMap<>();
-                    for (int i = 0; i < newFields.size(); i++) {
-                        map.put(newFields.get(i).name(), i);
-                    }
-
-                    int fieldIndex = map.get(move.fieldName());
-                    int refIndex = 0;
-                    if (move.type().equals(SchemaChange.Move.MoveType.FIRST)) {
-                        checkMoveIndexEqual(move, fieldIndex, refIndex);
-                        newFields.add(refIndex, newFields.remove(fieldIndex));
-                    } else if (move.type().equals(SchemaChange.Move.MoveType.AFTER)) {
-                        refIndex = map.get(move.referenceFieldName());
-                        checkMoveIndexEqual(move, fieldIndex, refIndex);
-                        if (fieldIndex > refIndex) {
-                            newFields.add(refIndex + 1, newFields.remove(fieldIndex));
-                        } else {
-                            newFields.add(refIndex, newFields.remove(fieldIndex));
-                        }
-                    }
-
+                    applyMove(newFields, move);
                 } else {
                     throw new UnsupportedOperationException(
                             "Unsupported change: " + change.getClass());
@@ -386,6 +365,61 @@ public class SchemaManager implements Serializable {
                 throw new RuntimeException(e);
             }
         }
+    }
+
+    public void applyMove(List<DataField> newFields, SchemaChange.Move move) {
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < newFields.size(); i++) {
+            map.put(newFields.get(i).name(), i);
+        }
+
+        int fieldIndex = map.getOrDefault(move.fieldName(), -1);
+        if (fieldIndex == -1) {
+            throw new IllegalArgumentException("Field name not found: " + move.fieldName());
+        }
+
+        // Get the reference index if the move is not FIRST or LAST
+        int refIndex =
+                (move.type() == SchemaChange.Move.MoveType.FIRST
+                                || move.type() == SchemaChange.Move.MoveType.LAST)
+                        ? 0
+                        : map.getOrDefault(move.referenceFieldName(), -1);
+        if (refIndex == -1) {
+            throw new IllegalArgumentException(
+                    "Reference field name not found: " + move.referenceFieldName());
+        }
+
+        // Check if the field to move is the same as the reference field
+        checkMoveIndexEqual(move, fieldIndex, refIndex);
+
+        // Handle the move operations
+        switch (move.type()) {
+            case FIRST:
+                moveField(newFields, fieldIndex, 0);
+                break;
+            case LAST:
+                moveField(newFields, fieldIndex, newFields.size() - 1);
+                break;
+            case AFTER:
+            case BEFORE:
+                int adjustment = move.type() == SchemaChange.Move.MoveType.AFTER ? 1 : 0;
+                int targetIndex =
+                        fieldIndex < refIndex ? refIndex - (1 - adjustment) : refIndex + adjustment;
+                moveField(newFields, fieldIndex, targetIndex);
+                break;
+        }
+    }
+
+    // Utility method to move a field within the list, handling range checks
+    private void moveField(List<DataField> newFields, int fromIndex, int toIndex) {
+        if (fromIndex < 0
+                || fromIndex >= newFields.size()
+                || toIndex < 0
+                || toIndex > newFields.size() - 1) {
+            return;
+        }
+        DataField fieldToMove = newFields.remove(fromIndex);
+        newFields.add(toIndex, fieldToMove);
     }
 
     public boolean mergeSchema(RowType rowType, boolean allowExplicitCast) {
